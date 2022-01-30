73cef40ff84428d776de39f79610ea4a4d8686a1
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-01-30 17:58:48 +0530

    Mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+const {Email,Student} = require('../Schema');
+let {mongodb,MongoClient,dbURL} = require('../dbConfig')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+mongoose.connect(dbURL)
+
+router.get('/', async(req,res)=>{
+    const emails = await Student.find();
+    res.send(emails)
+})
+
+
+router.post('/email',async(req,res)=>{
+  try{
+    const email= await Student.create(req.body)
+  res.send(email)
+}catch(error){
+      console.error(error._message);
+      res.send(error._message)
+}
+})
 
 module.exports = router;
